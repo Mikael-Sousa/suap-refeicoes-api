b0e3ea3ea627f017ae1a107e25a81a3f47699723
Mikael-Sousa <mikael.sr.dev@gmail.com>
2025-10-30 11:15:38 -0300

    feat: automatizacao das acoes necessarias para realizar uma reserva

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
 dotenv.config();
 const SUAP_USERNAME = process.env.SUAP_USERNAME;
@@ -15,16 +15,73 @@ const bot = async () => {
     const page = await browser.newPage();
     await page.goto(url);
 
-    await page.waitForSelector('input[name="username"]')
-    await page.waitForSelector('input[name="password"]')
+    await page.waitForSelector('input[name="username"]');
+    await page.waitForSelector('input[name="password"]');
 
-    await page.type('input[name="username"]', SUAP_USERNAME)
-    await page.type('input[name="password"]', SUAP_PASSWORD)
+    await page.type('input[name="username"]', SUAP_USERNAME);
+    await page.type('input[name="password"]', SUAP_PASSWORD);
 
-    await page.waitForSelector('input[value="Acessar"]')
+    await page.waitForSelector('input[value="Acessar"]');
 
-    await page.click('input[value="Acessar"')
-    
+    await page.click('input[value="Acessar"]');
+
+    await page.waitForSelector("a:has(span.fas.fa-chevron-right)", {
+      visible: true,
+    });
+
+    await page.click("a:has(span.fas.fa-chevron-right)");
+
+    await page.waitForSelector(
+      "ul:has(li.has-child.menu-atividades-estudantis)",
+      { visible: true }
+    );
+
+    await page.click("li.has-child.menu-atividades-estudantis");
+
+    await page.waitForSelector(
+      'li.has-child > a[title="Restaurante Institucional"]',
+      { visible: true }
+    );
+
+    await page.$eval(
+      'li.has-child > a[title="Restaurante Institucional"]',
+      (el) =>
+        el.scrollIntoView(
+          { behavior: "smooth", block: "center" },
+          { visible: true }
+        )
+    );
+
+    await page.click('li.has-child > a[title="Restaurante Institucional"]');
+
+    await page.waitForSelector(
+      'ul > li#menu-item-atividadesestudantis_restauranteinstitucional_reservarrefeições > a[title="Reservar Refeições"]',
+      { visible: true }
+    );
+
+    await page.$eval(
+      'li#menu-item-atividadesestudantis_restauranteinstitucional_reservarrefeições > a[title="Reservar Refeições"]',
+      (el) =>
+        el.scrollIntoView(
+          { behavior: "smooth", block: "center" },
+          { visible: true }
+        )
+    );
+
+    await page.click(
+      'li#menu-item-atividadesestudantis_restauranteinstitucional_reservarrefeições > a[title="Reservar Refeições"]'
+    );
+
+    await page.waitForSelector(".card-container > div:last-child", {
+      visible: true,
+    });
+    const mainDiv = await page.$(".card-container > div:last-child");
+
+    await page.evaluate((el) => {
+      const link = el.querySelector("ul > li > a");
+      if (link) link.scrollIntoView({ behavior: "smooth", block: "center" });
+      if (link) link.click();
+    }, mainDiv);
   } catch (err) {
     console.error("Erro no bot:", err.message);
     throw err;
